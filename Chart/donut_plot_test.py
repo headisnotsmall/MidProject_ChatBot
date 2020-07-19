d985b2052d455a1478039ffacb6736ffbc8a9a36
headisnotsmall <66876114+headisnotsmall@users.noreply.github.com>
2020-07-19 23:01:09 +0800

    update

--- Chart/donut_plot_test.py
+++ Chart/donut_plot_test.py
@@ -12,17 +12,18 @@ be_x = 0
 be_v = 0
 ju_x = 0
 ju_v = 0
+total = 0
 
 with open("i11price.csv") as f:
     dic = csv.DictReader(f)
     for row in dic:
-        if int(row["price"]) < 35900 - 359:
+        if int(row["price"]) < 35900 - 3590:
             below += 1
             if row["own"] == "1":
                 be_v += 1
             else:
                 be_x += 1
-        elif int(row["price"]) > 35900 + 359:
+        elif int(row["price"]) > 35900 + 3590:
             over += 1
             if row["own"] == "1":
                 ov_v += 1
@@ -34,6 +35,7 @@ with open("i11price.csv") as f:
                 ju_v += 1
             else:
                 ju_x += 1
+        total += 1
 print(just, ju_v, ju_x, over, ov_v, ov_x, below, be_v, be_x)
 
 # Make data: I have 3 groups and 7 subgroups
@@ -43,19 +45,23 @@ subgroup_names = ['own', "don't have", 'own', "don't have", 'own', "don't have"]
 subgroup_size = [ov_v, ov_x, be_v, be_x, ju_v, ju_x]
 
 # Create colors
-a, b, c = [plt.cm.Blues, plt.cm.Reds, plt.cm.Greens]
+a, b, c = [plt.cm.Purples, plt.cm.GnBu, plt.cm.Oranges]
 
 # First Ring (outside)
 fig, ax = plt.subplots()
 ax.axis('equal')
-mypie, _ = ax.pie(group_size, radius=1.3, labels=group_names, colors=[a(0.6), b(0.6), c(0.6)])
+mypie, _ = ax.pie(group_size, radius=1.3, labels=group_names, colors=[a(over / total), b(below / total), c(just / total)])
 plt.setp(mypie, width=0.3, edgecolor='white')
 
 # Second Ring (Inside)
 mypie2, _ = ax.pie(subgroup_size, radius=1.3 - 0.3, labels=subgroup_names, labeldistance=0.7,
-                   colors=[a(0.5), a(0.4), a(0.3), b(0.5), b(0.4), c(0.6), c(0.5), c(0.4), c(0.3), c(0.2)])
+                   colors=[a(ov_v / total), a(ov_x / total),
+                           b(be_v / total), b(be_x / total),
+                           c(ju_v / total), c(ju_x / total)])
 plt.setp(mypie2, width=0.4, edgecolor='white')
 plt.margins(0, 0)
 
+fig.savefig("chart.png")
+plt.close(fig)
 # show it
 plt.show()
---
+++ main.py
@@ -0,0 +1,178 @@
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage, FollowEvent, TemplateSendMessage,
+    MessageAction, URIAction, ButtonsTemplate,
+    QuickReply, QuickReplyButton, ImageSendMessage
+)
+
+import json
+
+app = Flask(__name__)
+
+line_bot_api = LineBotApi(
+    'nbE0Yqi3KGYDpWH579IgAtS1ggxub+PUUgy0tUvNlkKAfIoRdo4D2GOiaPOQgqA6wr47BQdZ/6S4C'
+    '/uhLmphZ2EpCs7xHhRwVC2kzpmdwVIyAnWRxdxEw3JKJvl1uY64mLntrp2GqPwUJccqeEc4owdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('784899c6df4a550a6b1d11f2fb5ad363')
+
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
+
+    return 'OK'
+
+# 歡迎訊息
+
+@handler.add(FollowEvent)
+def handle_follow(event):
+    user_profile = line_bot_api.get_profile(event.source.user_id)
+    with open("namelist.txt", "a") as myfile:
+        myfile.write(
+            json.dumps(
+                vars(user_profile)
+            )
+        )
+        myfile.write("\r")
+
+    follow_text_send_message = TextSendMessage(
+        "Hello，\n"
+        "我是「食米不知米價」機器人，\n"
+        "對你生活周遭的食衣住行開銷，\n"
+        "你到底了解了多少呢？\n"
+        "\n"
+        "先試試看你對甚麼領域有興趣吧！"
+    )
+
+# 詢問喜好
+
+    buttons_template_message = TemplateSendMessage(
+        alt_text='Buttons template',
+        template=ButtonsTemplate(
+            thumbnail_image_url='https://images.plurk.com/64ZDVCsKT7rRdkODQ9xWnN.jpg',
+            title='我想猜這個',
+            text='選一個有興趣的領域吧！',
+            actions=[
+                MessageAction(
+                    label='3C',  # 按鍵名稱
+                    text='我想猜猜3C產品'  # 點擊後、以用戶發的文字
+                ),
+                MessageAction(
+                    label='電玩',  # 按鍵名稱
+                    text='我想猜猜電玩產品'  # 點擊後、以用戶發的文字
+                ),
+                MessageAction(
+                    label='甜點',  # 按鍵名稱
+                    text='我想猜猜甜點價位'  # 點擊後、以用戶發的文字
+                )
+            ]
+        )
+    )
+
+    line_bot_api.reply_message(event.reply_token, [follow_text_send_message,
+                                                   buttons_template_message])
+
+
+# 詢問經驗
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_text_message(event):
+    if event.message.text == '我想猜猜3C產品':
+        text_quickreply1 = QuickReplyButton(action=MessageAction(label="有", text="我有iPhone"))
+        text_quickreply2 = QuickReplyButton(action=MessageAction(label="沒有", text="我沒有iPhone"))
+
+        quick_reply_array = QuickReply(items=[text_quickreply1, text_quickreply2])
+
+        reply_text_message = TextSendMessage("請問您是否持有iPhone手機？", quick_reply=quick_reply_array)
+
+        line_bot_api.reply_message(event.reply_token, [reply_text_message])
+
+    elif event.message.text == '我想猜猜電玩產品':
+        text_quickreply1 = QuickReplyButton(action=MessageAction(label="有", text="我有Switch"))
+        text_quickreply2 = QuickReplyButton(action=MessageAction(label="沒有", text="我沒有Switch"))
+
+        quick_reply_array = QuickReply(items=[text_quickreply1, text_quickreply2])
+
+        reply_text_message = TextSendMessage("請問您是否持有Switch主機？", quick_reply=quick_reply_array)
+
+        line_bot_api.reply_message(event.reply_token, [reply_text_message])
+
+    elif event.message.text in {"我有iPhone", "我沒有iPhone"}:
+        price_asking = TextSendMessage("猜猜看現在一台\n"
+                                       "iPhone 11 Pro 64G\n"
+                                       "售價大概多少錢呢？")
+        line_bot_api.reply_message(event.reply_token, price_asking)
+
+    elif event.message.text in {"我有Switch", "我沒有Switch"}:
+        price_asking = TextSendMessage("猜猜看現在一台\n"
+                                       "Switch紅藍款 (主機only)\n"
+                                       "售價大概多少錢呢？")
+        line_bot_api.reply_message(event.reply_token, price_asking)
+
+    # elif event.message.text == '我想猜猜甜點價位':
+    #     text_quickreply1 = QuickReplyButton(action=MessageAction(label="喜歡", text="我喜歡吃甜點"))
+    #     text_quickreply2 = QuickReplyButton(action=MessageAction(label="不喜歡", text="我不喜歡吃甜點"))
+    #
+    #     quick_reply_array = QuickReply(items=[text_quickreply1, text_quickreply2])
+    #
+    #     reply_text_message = TextSendMessage("請問您是否喜歡吃甜點？", quick_reply=quick_reply_array)
+    #
+    #     line_bot_api.reply_message(event.reply_token, [reply_text_message])
+
+# TODO 問題2 購買意願
+
+# TODO 估計價錢、回傳高估or低估 要用postback
+
+
+# @handler.add(MessageEvent, message=TextMessage)
+# def handle_message(event):
+#     if event.message.text in {"我有iPhone", "我沒有iPhone"}:
+#         price_asking = TextSendMessage("猜猜看現在一台\n"
+#                                        "iPhone 11 Pro 64G\n"
+#                                        "售價大概多少錢呢？")
+#         line_bot_api.reply_message(event.reply_token, price_asking)
+#     elif event.message.text in {"我有Switch", "我沒有Switch"}:
+#         price_asking = TextSendMessage("猜猜看現在一台\n"
+#                                        "Switch紅藍款 (主機only)\n"
+#                                        "售價大概多少錢呢？")
+#         line_bot_api.reply_message(event.reply_token, price_asking)
+
+    # elif event.message.text == "我喜歡吃甜點" or "我不喜歡吃甜點":
+    #     pass # todo 甜點資料
+    #     line_bot_api.reply_message(event.reply_token, [price_asking])
+
+# TODO 回傳統計圖表
+
+#
+# @handler.add(MessageEvent, message=TextMessage)
+# def handle_image_message(event):
+#     static_chart = ImageSendMessage(
+#         original_content_url="./chart.png",
+#         preview_image_url=""
+#     )
+#     line_bot_api.reply_message(event.reply_token, [static_chart])
+
+# TODO 圖文選單
+
+
+if __name__ == "__main__":
+    app.run()
