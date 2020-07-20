4fccbf0b7e3370acba2cefe215dccbde5f9a015a
Niels Liao <headinsotsmall@gmail.com>
2020-07-20 14:10:40 +0800

    make it better

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage, FollowEvent, TemplateSendMessage,
-    MessageAction, URIAction, ButtonsTemplate,
+    MessageEvent, TextMessage, TextSendMessage, FollowEvent, TemplateSendMessage, ConfirmTemplate,
+    MessageAction, URIAction, ButtonsTemplate, PostbackAction, Postback, PostbackEvent,
     QuickReply, QuickReplyButton, ImageSendMessage
 )
 
@@ -42,6 +42,7 @@ def callback():
 
 # 歡迎訊息
 
+
 @handler.add(FollowEvent)
 def handle_follow(event):
     user_profile = line_bot_api.get_profile(event.source.user_id)
@@ -71,17 +72,20 @@ def handle_follow(event):
             title='我想猜這個',
             text='選一個有興趣的領域吧！',
             actions=[
-                MessageAction(
-                    label='3C',  # 按鍵名稱
-                    text='我想猜猜3C產品'  # 點擊後、以用戶發的文字
+                PostbackAction(
+                    label='3c',
+                    display_text='我想猜猜3c產品',
+                    data='theme=1'
                 ),
-                MessageAction(
-                    label='電玩',  # 按鍵名稱
-                    text='我想猜猜電玩產品'  # 點擊後、以用戶發的文字
+                PostbackAction(
+                    label='電玩',
+                    display_text='我想猜猜看電玩價格',
+                    data='theme=2'
                 ),
-                MessageAction(
-                    label='甜點',  # 按鍵名稱
-                    text='我想猜猜甜點價位'  # 點擊後、以用戶發的文字
+                PostbackAction(
+                    label='甜點',
+                    display_text='我想猜猜甜點價位',
+                    data='theme=3'
                 )
             ]
         )
@@ -96,15 +100,33 @@ def handle_follow(event):
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_text_message(event):
-    if event.message.text == '我想猜猜3C產品':
-        text_quickreply1 = QuickReplyButton(action=MessageAction(label="有", text="我有iPhone"))
-        text_quickreply2 = QuickReplyButton(action=MessageAction(label="沒有", text="我沒有iPhone"))
-
-        quick_reply_array = QuickReply(items=[text_quickreply1, text_quickreply2])
-
-        reply_text_message = TextSendMessage("請問您是否持有iPhone手機？", quick_reply=quick_reply_array)
-
-        line_bot_api.reply_message(event.reply_token, [reply_text_message])
+    if PostbackEvent.data == 'theme=1':
+        confirm_template_message = TemplateSendMessage(
+            alt_text='你有用過iPhone嗎？',
+            template=ConfirmTemplate(
+                text='想了解你使用iPhone的經驗',
+                actions=[
+                    PostbackAction(
+                        label='有',
+                        display_text='我有用過iPhone',
+                        data='theme=1&have=1'
+                    ),
+                    PostbackAction(
+                        label='沒有',
+                        display_text='我梅有用過iPhone',
+                        data='theme=1&have=0'
+                    )
+                ]
+            )
+        )
+        # text_quickreply1 = QuickReplyButton(action=MessageAction(label="有", text="我有iPhone"))
+        # text_quickreply2 = QuickReplyButton(action=MessageAction(label="沒有", text="我沒有iPhone"))
+        #
+        # quick_reply_array = QuickReply(items=[text_quickreply1, text_quickreply2])
+        #
+        # reply_text_message = TextSendMessage("請問您是否持有iPhone手機？", quick_reply=quick_reply_array)
+        #
+        # line_bot_api.reply_message(event.reply_token, [reply_text_message])
 
     elif event.message.text == '我想猜猜電玩產品':
         text_quickreply1 = QuickReplyButton(action=MessageAction(label="有", text="我有Switch"))
@@ -145,7 +167,7 @@ def handle_text_message(event):
 
 # @handler.add(MessageEvent, message=TextMessage)
 # def handle_message(event):
-#     if event.message.text in {"我有iPhone", "我沒有iPhone"}:
+#     if event.message.text == "我有iPhone" or "我沒有iPhone":
 #         price_asking = TextSendMessage("猜猜看現在一台\n"
 #                                        "iPhone 11 Pro 64G\n"
 #                                        "售價大概多少錢呢？")
